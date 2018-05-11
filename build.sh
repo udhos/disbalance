87cb4b683a6e91ed8ba50d6bf841ea656d5ba998
udhos <everton.marques@gmail.com>
2018-05-11 03:29:23 -0300

    Do not generate cert.

--- build.sh
+++ build.sh
@@ -5,5 +5,3 @@ go tool fix ./*/*.go
 go tool vet ./disbalance
 go test -v ./disbalance
 go install ./disbalance
-
-echo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout key.pem -out cert.pem
