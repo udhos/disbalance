80569f8f2403fc7324025a511ca2c7d75b0ad44d
udhos <udhos>
2018-05-12 16:47:53 -0300

    Post.

--- disbalance/api.go
+++ disbalance/api.go
@@ -33,6 +33,12 @@ func registerApi(app *server, path string, handler apiHandler) {
 	app.apis = append(app.apis, path)
 }
 
+// https://stackoverflow.com/questions/630453/put-vs-post-in-rest
+//
+// POST to a URL creates a child resource at a server defined URL.
+// PUT to a URL creates/replaces the resource in its entirety at the client defined URL.
+// PATCH to a URL updates part of the resource at that client defined URL.
+
 func serveApiRule(w http.ResponseWriter, r *http.Request, app *server) {
 	log.Printf("serveApiRule: url=%s from=%s", r.URL.Path, r.RemoteAddr)
 
@@ -43,11 +49,31 @@ func serveApiRule(w http.ResponseWriter, r *http.Request, app *server) {
 	switch r.Method {
 	case http.MethodGet:
 		ruleGet(w, r, app)
+	case http.MethodDelete:
+		ruleDelete(w, r, app)
 	case http.MethodPost:
 		rulePost(w, r, app)
 	default:
 		http.Error(w, "Method not supported", 405)
 	}
+}
+
+func ruleDelete(w http.ResponseWriter, r *http.Request, app *server) {
+
+	name := strings.TrimPrefix(r.URL.Path, "/api/rule/")
+	if name == "" {
+		log.Printf("ruleDelete: missing name")
+		http.Error(w, "Missing rule name", 400)
+		return
+	}
+
+	if err := app.ruleDel(name); err != nil {
+		log.Printf("ruleDelete: %s: %v", name, err)
+		http.Error(w, "Not found", 404)
+		return
+	}
+
+	http.Error(w, "Rule "+name+" deleted", 200)
 }
 
 func ruleGet(w http.ResponseWriter, r *http.Request, app *server) {
@@ -93,7 +119,17 @@ func rulePost(w http.ResponseWriter, r *http.Request, app *server) {
 		return
 	}
 
-	writeBuf("rulePost", w, body)
+	var rules []rule
+
+	if errYaml := yaml.Unmarshal(body, &rules); errYaml != nil {
+		log.Printf("rulePost: unmarshal: %v", errYaml)
+		http.Error(w, "Bad request", 400)
+		return
+	}
+
+	app.rulePost(rules)
+
+	http.Error(w, fmt.Sprintf("Rules updated: %d", len(rules)), 200)
 }
 
 func serveApi(w http.ResponseWriter, r *http.Request, app *server) {
--- disbalance/server.go
+++ disbalance/server.go
@@ -41,18 +41,26 @@ type server struct {
 	lock       sync.RWMutex
 }
 
+// get a lock before calling unsafeSave
+// will read from shared config
+// will write into shared file
+func unsafeSave(cfg *config, configPath string) {
+	buf, errYaml := yaml.Marshal(cfg)
+	if errYaml != nil {
+		log.Printf("configSave: marshal: %s: %v", configPath, errYaml)
+		return
+	}
+	if err := ioutil.WriteFile(configPath, buf, 0777); err != nil {
+		log.Printf("configSave: %s: %v", configPath, err)
+	}
+	log.Printf("configSave: %s", configPath)
+}
+
 func (s *server) configSave() {
 	s.lock.Lock() // exclusive lock: will write on shared file s.configPath
 	defer s.lock.Unlock()
-	buf, errYaml := yaml.Marshal(s.cfg)
-	if errYaml != nil {
-		log.Printf("configSave: marshal: %s: %v", s.configPath, errYaml)
-		return
-	}
-	if err := ioutil.WriteFile(s.configPath, buf, 0777); err != nil {
-		log.Printf("configSave: %s: %v", s.configPath, err)
-	}
-	log.Printf("configSave: %s", s.configPath)
+
+	unsafeSave(&s.cfg, s.configPath)
 }
 
 func (s *server) configLoad() {
@@ -100,6 +108,51 @@ func (s *server) ruleGet(name string) (rule, error) {
 		}
 	}
 	return rule{}, fmt.Errorf("rule not found")
+}
+
+func unsafeDeleteRule(rules []rule, i int) {
+	// Delete without preserving order
+	// https://github.com/golang/go/wiki/SliceTricks#delete-without-preserving-order
+
+	newSize := len(rules) - 1
+	rules[i] = rules[newSize]
+	rules = rules[:newSize]
+}
+
+func (s *server) ruleDel(name string) error {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	for i, r := range s.cfg.Rules {
+		if r.Name == name {
+			unsafeDeleteRule(s.cfg.Rules, i)
+			unsafeSave(&s.cfg, s.configPath)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("rule not found")
+}
+
+func (s *server) rulePost(rules []rule) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	for _, newRule := range rules {
+		for old, oldRule := range s.cfg.Rules {
+			if newRule.Name == oldRule.Name {
+				// update old rule
+				log.Printf("rulePost FIXME WRITEME")
+				unsafeDeleteRule(s.cfg.Rules, old)
+				s.cfg.Rules = append(s.cfg.Rules, newRule)
+				continue
+			}
+			// append new rule
+			s.cfg.Rules = append(s.cfg.Rules, newRule)
+		}
+	}
+
+	unsafeSave(&s.cfg, s.configPath)
 }
 
 func auth(w http.ResponseWriter, r *http.Request, app *server) bool {
