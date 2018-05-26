7a47a01a97a9c953ae35f33860a43df39c5038a5
udhos <udhos>
2018-05-26 12:48:52 -0300

    Uninstall.

--- install.sh
+++ install.sh
@@ -1,20 +1,53 @@
 #!/bin/bash
 
+me=$(basename $0)
+
+msg() {
+	echo >&2 $me: $@
+}
+
 die() {
-	echo >&2 $(basename $0): $@
+	msg $@
 	exit 1
 }
 
-[ -f run/disbalance ] || die missing executable: run/disbalance
-[ -r run/disbalance ] || die missing executable: run/disbalance
-[ -x run/disbalance ] || die missing executable: run/disbalance
+do_install() {
+	[ -f run/disbalance ] || die missing executable: run/disbalance
+	[ -r run/disbalance ] || die missing executable: run/disbalance
+	[ -x run/disbalance ] || die missing executable: run/disbalance
 
-mkdir -p /var/run/disbalance
+	[ -f /usr/local/sbin/disbalance ] && die already installed, use: $0 remove
 
-cp -p -r run/console            /var/run/disbalance
-cp -p    run/disbalance.service /var/run/disbalance
-cp -p    run/disbalance         /usr/local/sbin/disbalance
+	mkdir -p /var/run/disbalance
 
-systemctl reenable /var/run/disbalance/disbalance.service
-systemctl reload disbalance.service
-systemctl restart disbalance.service
+	cp -r run/console            /var/run/disbalance
+	cp    run/disbalance         /usr/local/sbin/disbalance
+	cp    run/disbalance.service /lib/systemd/system
+
+	systemctl daemon-reload
+	systemctl enable disbalance.service
+	systemctl reload-or-restart disbalance.service
+}
+
+do_uninstall() {
+	systemctl daemon-reload
+	systemctl stop disbalance.service
+	systemctl disable disbalance.service
+	pkill -x -9 disbalance
+	rm -f /usr/local/sbin/disbalance
+}
+
+case "$1" in
+	remove)
+		do_uninstall
+	;;
+	'')
+		do_install
+	;;
+	*)
+		msg invalid argument: "$1"
+		echo >&2 usage: $(basename $0) [remove]
+		exit 2
+	;;
+esac
+
